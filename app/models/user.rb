46f13cec78a254f5e655ada3479951db99032d01
Donovan Jones <donovanljones94@gmail.com>
2019-12-02 14:52:09 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User
   attr_reader :username, :password 
   attr_accessor :balance
   
-  def initialize ()
+  def initialize(args)
+    @username = args[:username]
+    @password = args[:password]
+    @balance = args[:balance]
+  end
   
   
 end
